468684f4be1f09e8d87ca17058d4971a19a97c99
Matías Cruz <matias18233@gmail.com>
2021-07-07 22:54:56 -0300

    Primer commit

---
+++ Assets/Scripts/Editor/Tests/CharacterTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CharacterTest
+    {
+        Character character;
+        Character target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            character = new Character();
+            
+        }
+
+        [Test]
+        public void CharacterTestHealth()
+        {
+
+
+            Assert.AreEqual(1000, character.Health);
+        }
+
+        [Test]
+        public void CharacterTestLevel()
+        {
+            Assert.AreEqual(1, character.Level);
+        }
+
+        [Test]
+        public void CharacterTestAlive()
+        {
+            Assert.IsTrue(character.Alive);
+        }
+
+        [Test]
+        public void CharacterTakeDamage()
+        {
+            int initialHealth = character.Health;
+
+            character.TakeDamage(50);
+
+            Assert.AreEqual(initialHealth - 50, character.Health);
+        }
+
+        [Test]
+        public void CharacterDealDamage()
+        {
+            target = new Character();
+
+            int initialHealth = target.Health;
+
+            character.DealDamage(target);
+
+            Assert.AreEqual(initialHealth - character.AttackDamage, target.Health);
+        }
+
+        [Test]
+        public void CharactersDiesOnHealthBelow0()
+        {
+            int initialHealth = character.Health;
+
+            character.TakeDamage(initialHealth + 1);
+
+            Assert.AreEqual(0, character.Health);
+
+            Assert.IsFalse(character.Alive);
+        }
+
+        [Test]
+        public void CharacterTakeHeal()
+        {
+            character.TakeDamage(50);
+            int initialHealth = character.Health;
+
+            character.TakeHeal(50);
+
+            Assert.AreEqual(initialHealth + 50, character.Health);
+        }
+
+        [Test]
+        public void CharacterCannotHealIsDead()
+        {
+            character.TakeDamage(character.Health);
+            int initialHealth = character.Health;
+
+            character.TakeHeal(50);
+
+            Assert.AreEqual(initialHealth, character.Health);
+        }
+
+        [Test]
+        public void CharacterMaxHeal()
+        {
+            character.TakeDamage(50);
+
+            character.TakeHeal(51);
+
+            Assert.AreEqual(character.MaxHealth, character.Health);
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Character.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public int MaxHealth { get; set; }
+    public int Health { get; set; }
+    public int Level { get; private set; }
+    public bool Alive { get; set; }
+
+    public int AttackDamage { get; set; }
+
+    public Character()
+    {
+        MaxHealth = 1000;
+        Health = MaxHealth;
+        Level = 1;
+        Alive = true;
+        
+        AttackDamage = 50;
+    }
+
+    public void DealDamage(Character target)
+    {
+
+        target.TakeDamage(AttackDamage);
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+        Health -= damage;
+        if (Health <= 0)
+        {
+            Health = 0;
+            Alive = false;
+        }
+    }
+
+    public void TakeHeal(int heal)
+    {
+        if (!Alive) return;
+
+        Health += heal;
+
+        if (Health > MaxHealth)
+            Health = MaxHealth;
+    }
+}
